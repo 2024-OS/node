7c4d7274a990cdaa21d754c78ee58dfcad30a59d
jyzzang02 <youngzzang02@naver.com>
2024-11-18 02:33:35 +0900

    feat: 하트에 점수추가
    점수를 추가해서 투표기능 비슷하게 만들었습니다.

--- app/src/main/java/com/example/node_project/PlanItem.kt
+++ app/src/main/java/com/example/node_project/PlanItem.kt
@@ -1,7 +1,8 @@
 package com.example.node_project
 
 data class PlanItem(
-    val title: String,               // 계획 제목
-    val description: String?,        // 계획 설명 (선택적)
-    var isChecked: Boolean = false   // 체크박스 상태
+    var title: String = "",             // 계획 제목
+    var description: String = "",       // 계획 설명
+    var isChecked: Boolean = false,     // 체크박스 상태
+    var score: Int = 0                   // 점수
 )
