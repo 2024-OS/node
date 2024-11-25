91fa64c1ede3b345586c2256430232ab735316ae
kangmin1213 <kangmin2891@gmail.com>
2024-11-26 01:56:59 +0900

    feat: MVVM 추가
    캘린더 페이지에 MVVM기능을 추가하였습니다.
    즉, 캘린더를 날짜와 리스트를 분리하여, 독립적으로 코드를 수정할 수 있도록 하였습니다.

--- app/src/main/java/com/example/node_project/ScheduleItem.kt
+++ app/src/main/java/com/example/node_project/ScheduleItem.kt
@@ -1,3 +1,6 @@
 package com.example.node_project.models
 
-data class ScheduleItem(var task: String, var isChecked: Boolean = false)
+data class ScheduleItem(
+    var task: String,
+    var isChecked: Boolean = false
+)
